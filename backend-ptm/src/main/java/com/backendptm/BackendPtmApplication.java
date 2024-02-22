7362ab6ddf6dbc367b06a20c91d0629debdbe73b
Jean Lozano <michecoanexa52@gmail.com>
2024-02-21 21:08:20 -0500

    Start project

---
+++ backend-ptm/src/main/java/com/backendptm/BackendPtmApplication.java
@@ -0,0 +1,13 @@
+package com.backendptm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPtmApplication.class, args);
+    }
+
+}
---
+++ backend-ptm/src/test/java/com/backendptm/BackendPtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
