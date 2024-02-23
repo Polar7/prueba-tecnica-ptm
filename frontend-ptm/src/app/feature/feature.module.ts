83dbc05629c36ead785869941645115fa3bdfba7
Jean Lozano <michecoanexa52@gmail.com>
2024-02-22 22:55:15 -0500

    Creacion de la tabla de productos, llamada a la funcion combinatoria y habilitacion de CORS

--- frontend-ptm/src/app/feature/feature.module.ts
+++ frontend-ptm/src/app/feature/feature.module.ts
@@ -5,6 +5,7 @@ import { FeatureRoutingModule } from './feature-routing.module';
 import { PortfolioComponent } from './home/pages/portfolio/portfolio.component';
 import { FooterComponent } from './layout/components/footer/footer.component';
 import { HeaderComponent } from './layout/components/header/header.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { HeaderComponent } from './layout/components/header/header.component';
   imports: [
     CommonModule,
     FeatureRoutingModule,
+    FormsModule,
   ]
 })
 export class FeatureModule { }
--- frontend-ptm/src/app/feature/home/pages/portfolio/portfolio.component.ts
+++ frontend-ptm/src/app/feature/home/pages/portfolio/portfolio.component.ts
@@ -3,6 +3,7 @@ import {ProductDto} from "../../../../core/dto/productDto";
 import {ProductService} from "../../../../core/service/product.service";
 import {MeowFactsService} from "../../../../core/service/meow-facts.service";
 import Swal from "sweetalert2";
+import {CombinationDto} from "../../../../core/dto/combinationDto";
 
 /**
  * Componente general donde se visualizan todos los productos
@@ -24,6 +25,21 @@ export class PortfolioComponent implements OnInit, AfterViewInit {
    */
   public meowFacts: Array<string>;
 
+  /**
+   * Valor para el filtro de la combinacion de productos
+   */
+  public valueFilterCombination: number;
+
+  /**
+   * Lista de las combinaciones de los productos
+   */
+  public combinationList: Array<CombinationDto>;
+
+  /**
+   * Construye el componente
+   * @param productService Servicio de producto
+   * @param meowFactService Servicio de datos de gatos
+   */
   constructor(private productService: ProductService, private meowFactService: MeowFactsService) {
   }
 
@@ -44,6 +60,47 @@ export class PortfolioComponent implements OnInit, AfterViewInit {
    */
   ngAfterViewInit(): void {
     this.productService.getAllProducts().subscribe(x => this.allProducts = x);
+  }
+
+  /**
+   * Elimina un producto seleccionado en la tabla
+   * @param idProduct ID del producto a eliminar
+   */
+  public deleteProduct(idProduct: number): void {
+    Swal.fire({
+      title: "Est&aacute;s seguro de eliminar este producto?",
+      showDenyButton: true,
+      confirmButtonText: "Si, eliminar",
+      confirmButtonColor: "#013F4E",
+      denyButtonText: `Cancelar`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.productService.deleteProduct(idProduct).subscribe({
+          next: value => {
+            if (value) {
+              Swal.fire({
+                title: "Producto eliminado correctamente",
+                icon: "success",
+                confirmButtonColor: "#013F4E",
+              });
+            } else {
+              Swal.fire({
+                title: "Error t&eacute;cnico al eliminar el producto",
+                icon: "success",
+                confirmButtonColor: "#013F4E",
+              });
+            }
+          }
+        });
+      }
+    });
+  }
+
+  /**
+   * Busca las combinaciones posibles segun el valor ingresado para el filtro
+   */
+  public searchCombinationProducts(): void {
+    this.productService.getCombinationProducts(this.valueFilterCombination).subscribe(x => this.combinationList = x);
   }
 
   /**
